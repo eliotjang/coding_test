b5f460469fe658fef92f5877036b8b659bc073b6
eliotjang <eliotjang2@gmail.com>
2019-11-11 23:06:41 +0900

    copy code... :(

--- Programmers/Stack&Queue/PrinterV2.py
+++ Programmers/Stack&Queue/PrinterV2.py
@@ -1,24 +1,37 @@
 def solution(priorities, location):
+    max = 0
+    tmp = 0
     answer = 0
-    search, c = sorted(priorities, reverse=True), 0
-    while True:
-        for i, priority in enumerate(priorities):
-            s = search[c]
-            if priority == s:
-                c += 1
-                answer += 1
-                if i == location:
-                    break
-        else:
-            continue
-        break
+    while(True):
+        for i in priorities:
+            if i > max:
+                max = i
+        tmp = priorities[0]
+        if tmp < max:
+            if location == 0:
+                location = len(priorities) - 1
+            else:
+                location -= 1
+            priorities.remove(tmp)
+            priorities.append(tmp)
+        elif tmp == max:
+            priorities.remove(tmp)
+            answer += 1
+            max = 0
+            for i in priorities:
+                if i > max:
+                    max = i
+            if location == 0:
+                break
+            else:
+                location -= 1
     return answer
 
 if __name__ == "__main__":
-    #priorities = [2, 1, 3, 2]
-    priorities = [1, 1, 9, 1, 1, 1]
+    priorities = [2, 1, 3, 2]
+    #priorities = [1, 1, 9, 1, 1, 1]
     #priorities = [1, 1, 9, 1, 9, 1]
-    #location = 2
-    location = 0
+    location = 2
+    #location = 0
     #location = 4
     print(solution(priorities, location))
