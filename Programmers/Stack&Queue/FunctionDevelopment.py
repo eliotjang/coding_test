09ad0ef5774a87af2e58e53d11dc3e31a08e83b6
eliotjang <eliotjang2@gmail.com>
2019-11-11 19:20:24 +0900

    complete my code!! :D

--- Programmers/Stack&Queue/FunctionDevelopment.py
+++ Programmers/Stack&Queue/FunctionDevelopment.py
@@ -1,6 +1,29 @@
 def solution(progresses, speeds):
     answer = []
+    days = []
+    for i in range(len(progresses)):
+        count = 0
+        tmp = 100 - progresses[i]
+        while tmp > 0:
+            tmp = tmp - speeds[i]
+            count += 1
+        if not days:
+            days.append(count)
+        elif max(days) > count:
+            days.append(max(days))
+        else:
+            days.append(count)
+    
+    while days:
+        y = min(days)
+        x = days.count(y)
+        answer.append(x)
+        while y in days:
+            days.remove(y)
+
     return answer
 
 if __name__ == "__main__":
-    pass
+    progresses = [93,30,55]
+    speeds = [1,30,5]
+    print(solution(progresses, speeds))
