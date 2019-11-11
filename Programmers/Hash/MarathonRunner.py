9fbfa78f2a3b287d70d776e4d61ec27fe3fb7f9f
eliotjang <eliotjang2@gmail.com>
2019-11-11 17:00:26 +0900

    for hash level 1 program

--- Programmers/Hash/MarathonRunner.py
+++ Programmers/Hash/MarathonRunner.py
@@ -1,6 +1,18 @@
 def solution(participant, completion):
-    answer = ''
+    tmp = participant.copy()
+    for i in completion:
+        for j in participant:
+            if i is j:
+                tmp.remove(j)
+                break
+    answer = tmp[0]
     return answer
 
 if __name__ == "__main__":
-    pass
+    #participant = ["leo", "kiki", "eden"]
+    #completion = ["eden", "kiki"]
+    #participant = ["marina", "josipa", "nikola", "vinko", "filipa"]
+    #completion = ["josipa", "filipa", "marina", "nikola"]
+    participant = ["mislav", "stanko", "mislav", "ana"]
+    completion = ["stanko", "ana", "mislav"]
+    print(solution(participant, completion))
