2112af4c4ae295b88cd3099f81d0ebf9e2bf2d34
eliotjang <eliotjang2@gmail.com>
2019-11-11 22:01:05 +0900

    modified code... not work well. just for test case

--- Programmers/Stack&Queue/Printer.py
+++ Programmers/Stack&Queue/Printer.py
@@ -1,8 +1,24 @@
 def solution(priorities, location):
-    answer = 0
+    search = priorities[location]
+    tmp = list(priorities)
+    big = max(priorities)
+    count = 0
+    for i in range(len(priorities)):
+        if priorities[i] < big:
+            count += 1
+            tmp.append(priorities[i])
+            del tmp[0]
+        else:
+            break
+    x = location - count
+    if x < 0:
+        x = x + len(tmp)
+    answer = x + 1
     return answer
 
 if __name__ == "__main__":
-    priorities = [2, 1, 3, 2]
-    location = 2
+    #priorities = [2, 1, 3, 2]
+    priorities = [1, 1, 9, 1, 1, 1]
+    #location = 2
+    location = 0
     print(solution(priorities, location))
