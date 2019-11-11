d57c4f7993c0c5aeb8c32420104ff4bd019f87eb
eliotjang <eliotjang2@gmail.com>
2019-11-12 00:00:44 +0900

    initial commit

---
+++ Programmers/Stack&Queue/BridgePassingTruck.py
@@ -0,0 +1,9 @@
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    return answer
+
+if __name__ == "__main__":
+    bridge_length = 2
+    weight= 10
+    truck_weights = [7,4,5,6]
+    print(solution(bridge_length, weight, truck_weights))
