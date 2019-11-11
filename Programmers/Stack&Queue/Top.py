ab629fa728966c65edef68bea7a9cdfcb4f50cb6
eliotjang <eliotjang2@gmail.com>
2019-11-11 23:53:01 +0900

    complete code! :D

--- Programmers/Stack&Queue/Top.py
+++ Programmers/Stack&Queue/Top.py
@@ -1,5 +1,15 @@
 def solution(heights):
-    answer = []
+
+    answer = [0] * len(heights)
+    for i in range(len(heights)-1, 0, -1):
+        j = len(heights)-1
+        while j > 0:
+            if heights[i] < heights[j-1]:
+                del answer[i]
+                answer.insert(i, j)
+                break
+            j -= 1
+        x = heights.pop()
     return answer
 
 if __name__ == "__main__":
