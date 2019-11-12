e1b10cbd6485a983951d4348394b173933549c13
eliotjang <eliotjang2@gmail.com>
2019-11-12 22:27:15 +0900

    not work..

--- Programmers/Stack&Queue/StockPrice.py
+++ Programmers/Stack&Queue/StockPrice.py
@@ -1,4 +1,28 @@
+def solution(prices):
+    timecount = [0] * len(prices)
+    height = 0
+    count = 0
+    time = 0
+    j = 0
+    while prices:
+        x = prices.pop(0)
+        time += 1
+        if x > height:
+            timecount.insert(count, time)
+            time = 0
+        elif x <= height:
+            j = 0
+            while prices[j] >= height:
+                time += 1
 
+                j += 1
+            time += 1
+            
+            
+
+    answer = []
+    return answer
 
 if __name__ == "__main__":
-    pass
+    prices = [1, 2, 3, 2, 3]
+    print(solution(prices))
