a0115d83ef10b75f45f7525a08ef63db3a29f11c
eliotjang <eliotjang2@gmail.com>
2019-11-13 00:56:52 +0900

    copy code... :(

---
+++ Programmers/Stack&Queue/StockPriceV2.py
@@ -0,0 +1,15 @@
+def solution(prices):
+    answer = [0] * len(prices)
+    for i in range(len(prices)):
+        for j in range(i+1, len(prices)):
+            if prices[i] <= prices[j]:
+                answer[i] += 1
+            else:
+                answer[i] += 1
+                break
+    return answer
+
+if __name__ == "__main__":
+    prices = [1, 2, 3, 2, 3]
+    #prices = [1, 2, 1, 5, 4, 3, 4, 1, 2]
+    print(solution(prices))
