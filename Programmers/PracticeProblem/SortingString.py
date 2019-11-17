45e0c00417e63916596c7ac7d88941349e2c7594
eliotjang <eliotjang2@gmail.com>
2019-11-17 23:55:07 +0900

    initial commit'

---
+++ Programmers/PracticeProblem/SortingString.py
@@ -0,0 +1,10 @@
+def solution(strings, n):
+    for string in strings:
+        pass
+    answer = []
+    return answer
+
+if __name__ == "__main__":
+    strings = ['sun', 'bed', 'car']
+    n = 1
+    print(solution(strings, n))
