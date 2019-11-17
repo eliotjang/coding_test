0a053f1738236b595ac25595e40c884d3df9ca9b
eliotjang <eliotjang2@gmail.com>
2019-11-17 20:35:25 +0900

    failed code... :(

--- Programmers/BruteSearch/FindPrimeNum.py
+++ Programmers/BruteSearch/FindPrimeNum.py
@@ -1,6 +1,36 @@
 def solution(numbers):
+    fornum = list(numbers)
+    countnum = list(numbers)
+    count = 0
     answer = 0
+    tmp = ''
+    for idx, number in enumerate(numbers):
+        countnum = list(numbers)
+        del countnum[idx]
+        tmp = number
+        while countnum:
+            answer += find_prime(tmp)
+            if not countnum:
+                break
+            tmp += countnum[count]
+            count += 1
+        
+
     return answer
+
+def find_prime(strnum):
+    x = int(strnum)
+    if x > 1:
+        for i in range(2,x):
+            if (x % i) == 0:
+                result = 1
+                break
+        else:
+            result = 0
+    else:
+        result = 0
+    return result
+
 
 if __name__ == "__main__":
     numbers = "17"
