6170778067231be89fdca9936138b458c837e2be
eliotjang <eliotjang2@gmail.com>
2019-11-20 17:12:29 +0900

    initial commit

---
+++ Programmers/PracticeProblem/FindKim.py
@@ -0,0 +1,10 @@
+def solution(seoul):
+    answer = ''
+    if seoul in 'Kim':
+        x = seoul.index('Kim')
+    
+    return x
+
+if __name__ == "__main__":
+    seoul = ['Jane', 'Kim']
+    print(solution(seoul))
