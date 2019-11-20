a95f8140f44bf7501c155e2fb2fc3a0a3bd67d27
eliotjang <eliotjang2@gmail.com>
2019-11-20 17:38:40 +0900

    complete code!

--- Programmers/PracticeProblem/GetMidChar.py
+++ Programmers/PracticeProblem/GetMidChar.py
@@ -1,7 +1,11 @@
 def solution(s):
-    answer = ''
+    if len(s)%2 == 0:
+        answer = s[int(len(s)/2)-1:int(len(s)/2)+1]
+    else:
+        answer = s[int(len(s)/2):int(len(s)/2)+1]
     return answer
 
 if __name__ == "__main__":
-    s = 'abcde'
+    #s = 'abcde'
+    s = 'qwer'
     print(solution(s))
