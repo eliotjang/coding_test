1d5a51fa2d3835588a1cf6cd3877ea5ebb7c2604
eliotjang <eliotjang2@gmail.com>
2019-11-23 01:59:58 +0900

    not work

--- Programmers/PracticeProblem/WantSortingString.py
+++ Programmers/PracticeProblem/WantSortingString.py
@@ -1,7 +1,9 @@
 def solution(strings, n):
-    answer = []
+
+    answer = list(map(sorted, strings))
     return answer
 
 if __name__ == '__main__':
     strings = ['sun', 'bed', 'car']
-    
+    n = 1
+    solution(strings, n)
