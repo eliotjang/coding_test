f7c619735eb2a179306bc83b05261fc7977575a2
eliotjang <eliotjang2@gmail.com>
2019-11-23 03:30:49 +0900

    complete code

--- Programmers/PracticeProblem/WantSortingString.py
+++ Programmers/PracticeProblem/WantSortingString.py
@@ -1,9 +1,17 @@
 def solution(strings, n):
+    tmp = []
+    answer = []
+    for i, x in enumerate(strings):
+        tmp.append(x[n] + x)
     
-    answer = list(map(sorted, strings))
+    tmp.sort()
+    for j, y in enumerate(tmp):
+        answer.append(y[1:])
     return answer
 
 if __name__ == '__main__':
-    strings = ['sun', 'bed', 'car']
-    n = 1
+    #strings = ['sun', 'bed', 'car']
+    strings = ['abce', 'abcd', 'cdx']	
+    #n = 1
+    n = 2
     solution(strings, n)
