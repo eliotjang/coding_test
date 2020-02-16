06300ddb15b0808a0fd98d0fb97e03866efb6ec6
eliotjang <eliotjang2@gmail.com>
2020-02-16 23:50:54 +0900

    for test

--- HackerRank/Data_Structures/Trees/Level_Order_Traversal_archive/Level_Order_Traversal.cpp
+++ HackerRank/Data_Structures/Trees/Level_Order_Traversal_archive/Level_Order_Traversal.cpp
@@ -50,7 +50,8 @@ class Node {
         //TODO: write some codes.
         queue<int> q;
         q.push(10);
-        q.pop(10); 
+        //q.pop(10);
+        int a = 0; 
     }
 
 }; //End of Solution
