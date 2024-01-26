626d4426cec755e18daf8ace8e6bd093af0f68ad
Eliot Jang <eliotjang2@gmail.com>
2024-01-27 00:56:57 +0900

    add one_dimensional_array code
    in Baekjoon

---
+++ Baekjoon/coding_basic/one_dimensional_array/2562.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// 포인터 사용
+/* int main()
+{
+    // freopen("input.txt","r",stdin);
+    int tmp = 0;
+    int* pArr = new int[9];
+    for (int i=0; i<9; i++) {
+        cin >> *(pArr + i);
+    }
+    for (int i=1; i<9; i++) {
+        if(*(pArr + tmp) < *(pArr + i)) {
+            tmp = i;
+        }
+    }
+    cout << *(pArr + tmp) << '\n' << tmp+1 << endl;
+
+    delete[] pArr;
+    return 0;
+} */
+
+//vector 컨테이너 사용
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    return 0;
+}
