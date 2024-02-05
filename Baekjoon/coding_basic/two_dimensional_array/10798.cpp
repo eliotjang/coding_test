6daf0b1a9f94aa9c4f175e6e8f61c5de3a70c199
Eliot Jang <eliotjang2@gmail.com>
2024-02-06 00:10:47 +0900

    add read column code

--- Baekjoon/coding_basic/two_dimensional_array/10798.cpp
+++ Baekjoon/coding_basic/two_dimensional_array/10798.cpp
@@ -4,20 +4,25 @@ using namespace std;
 
 int main() 
 {
-    freopen("input.txt","r",stdin);
-    vector<vector<char>> vt(5);
+    // freopen("input.txt","r",stdin);
+    vector<vector<char>> vt(5, vector<char>(15, '~'));
+    int t = 0;
     for (int i=0; i<5; i++) {
         string s;
         getline(cin, s);
+        if (t < s.size()) {
+            t = s.size();
+        }
         for (int j=0; j<s.size(); j++) {
-            vt[i].push_back(s[j]);
+            vt[i][j] = s[j];
         }
     }
-    for (int i=0; i<5; i++) {
-        for (int j=0; j<5; j++) {
-            cout << vt[j][i];
+    for (int i=0; i<t; i++) {
+        for (int j=0; j<vt.size(); j++) {
+            if (vt[j][i] != '~') {
+                cout << vt[j][i];
+            }   
         }
-        cout << endl;
     }
     return 0;
 }
