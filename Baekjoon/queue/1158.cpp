437aa7fd308c9350e6a16bdda21df4d7f1cc2d72
Eliot Jang <eliotjang2@gmail.com>
2024-02-20 23:59:29 +0900

    test code

--- Baekjoon/queue/1158.cpp
+++ Baekjoon/queue/1158.cpp
@@ -1,53 +1,21 @@
-#include <iostream>
+#include <cstdio>
+#include <queue>
 using namespace std;
 
-struct Queue {
-    int data[5000];
-    int begin;
-    int end;
-    int count;
-    int pushdata;
-    int flag;
-    Queue(int num) {
-        begin = 0;
-        end = 0;
-        pushdata = 1;
-        count = num;
-        flag = 0;
-    }
-    void push() {
-        data[end] = pushdata++;
-        end++;
-    }
-    bool empty() {
-        if (begin == end) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    // 1 2 3 4 5 6 7
-    int pop() {
-        int tmp = count;
-        while (tmp) {
-            if (data[flag] != 0) {
-                flag++;
-                tmp--;
-            } else continue;
-            // 0이면 넘기고 아니면 K번 넘기고 거기있는 값 출력
-        }
-    }
-};
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main()
+{
     freopen("input.txt","r",stdin);
     int N, K;
-    cin >> N >> K;
-    Queue queue(K);
-    while(N--) {
-        queue.push();
+    scanf("%d %d", &N, &K);
+    queue<int> q;
+    for(int i=0; i<N; i++) {
+        q.push(i+1);
+    }
+    printf("<");
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<K; j++) {
+            //push and pop
+        }
     }
     return 0;
 }
