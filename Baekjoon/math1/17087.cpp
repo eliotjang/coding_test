3b5688bda7ff49ef681d2ccac3f33c5eb8cab6f9
Eliot Jang <eliotjang2@gmail.com>
2024-02-26 15:57:04 +0900

    add base code

--- Baekjoon/math1/17087.cpp
+++ Baekjoon/math1/17087.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+long long GCD(long long a, long long b)
+{
+    while (b != 0) {
+        int r = a%b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
 
 int main()
 {
     freopen("input.txt","r",stdin);
-
+    int n;
+    long long s, tmp;
+    long long result = 0;
+    cin >> n >> s;
+    vector<long long> A(n);
+    for (int i=0; i<n; i++) {
+        cin >> tmp;
+        tmp -= s;
+        A[abs(tmp)];
+    }
+    result = A[0];
+    for (int i=1; i<A.size(); i++) {
+        if (result > A[i]) {
+            result = GCD(result, A[i]);
+        } else {
+            result = GCD(A[i], result);
+        }
+    }
+    cout << result << endl;
     return 0;
 }
