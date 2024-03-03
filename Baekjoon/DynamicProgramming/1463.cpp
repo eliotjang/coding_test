3b22c79c06b03965bfd12192c9e0ca2be455096b
Eliot Jang <eliotjang2@gmail.com>
2024-03-03 23:59:42 +0900

    use memoization

--- Baekjoon/DynamicProgramming/1463.cpp
+++ Baekjoon/DynamicProgramming/1463.cpp
@@ -7,10 +7,17 @@ int main()
     int memo[1000000] = {0,};
     int N;
     cin >> N;
-    // D[n] = min(D[n-1], D[n/3], D[n/2])
+    // D[n] = min(D[n-1], D[n/3], D[n/2]) + 1
     if (N == 1) return 1;
     for (int i = 2; i<N; i++) {
-        
+        memo[i] = memo[i-1] + 1;
+        if (memo[i] > 0) return memo[i];
+        if (i%2 == 0) {
+
+        }
+        if (i%3 == 0) {
+            
+        }
     }
     return 0;
 }
