deca652917711c39abd1adf9d73f16ec244a9d02
Eliot Jang <eliotjang2@gmail.com>
2024-03-04 00:50:13 +0900

    add make to one (Dynamic Programming) with Memoization & Bottom Up

--- Baekjoon/DynamicProgramming/1463.cpp
+++ Baekjoon/DynamicProgramming/1463.cpp
@@ -3,21 +3,25 @@ using namespace std;
 
 int main()
 {
-    freopen("input.txt","r",stdin);
-    int memo[1000000] = {0,};
+    // freopen("input.txt","r",stdin);
     int N;
     cin >> N;
+    int* memo = new int[N+1]();
     // D[n] = min(D[n-1], D[n/3], D[n/2]) + 1
-    if (N == 1) return 1;
-    for (int i = 2; i<N; i++) {
+    if (N == 1) {
+        cout << 0 << endl;
+        return 0;
+    }
+    for (int i = 2; i<=N; i++) {
         memo[i] = memo[i-1] + 1;
-        if (memo[i] > 0) return memo[i];
-        if (i%2 == 0) {
-
+        if (i%2 == 0 && memo[i] > memo[i/2] + 1) {
+            memo[i] = memo[i/2] + 1;
         }
-        if (i%3 == 0) {
-            
+        if (i%3 == 0 && memo[i] > memo[i/3] + 1) {
+            memo[i] = memo[i/3] + 1;
         }
     }
+    cout << memo[N] << endl;
+    delete[] memo;
     return 0;
 }
