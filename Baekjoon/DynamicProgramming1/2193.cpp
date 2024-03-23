0966762a7e32c62850d0178c6c4ccb38ea045ed6
Eliot Jang <eliotjang2@gmail.com>
2024-03-23 16:39:52 +0900

    add base code

--- Baekjoon/DynamicProgramming1/2193.cpp
+++ Baekjoon/DynamicProgramming1/2193.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    freopen("input.txt","r",stdin);
+    // freopen("input.txt","r",stdin);
     int N;
     cin >> N;
     vector<vector<int>> D(N+1, vector<int>(2));
@@ -15,10 +15,10 @@ int main()
     for (int i=2; i<=N; i++) {
         for (int j=0; j<2; j++){
             if (j == 0) {
-                D[i][j] = D[N-1][j] + D[N-1][1];
+                D[i][j] = D[i-1][j] + D[i-1][1];
             }
             if (j == 1) {
-                D[i][j] = D[N-1][0];
+                D[i][j] = D[i-1][0];
             }
         }
     }
