d1b10822c4f6473ef1f3e479db53a57400f0d2b9
Eliot Jang <eliotjang2@gmail.com>
2024-03-24 20:32:02 +0900

    add "이친수" (Dynamic Programming)

--- Baekjoon/DynamicProgramming1/2193.cpp
+++ Baekjoon/DynamicProgramming1/2193.cpp
@@ -7,7 +7,7 @@ int main()
     // freopen("input.txt","r",stdin);
     int N;
     cin >> N;
-    vector<vector<int>> D(N+1, vector<int>(2));
+    vector<vector<long long>> D(N+1, vector<long long>(2));
     D[1][0] = 0;
     D[1][1] = 1;
     // D[N][0] = D[N-1][0] + D[N-1][1]
@@ -15,10 +15,10 @@ int main()
     for (int i=2; i<=N; i++) {
         for (int j=0; j<2; j++){
             if (j == 0) {
-                D[i][j] = D[i-1][j] + D[i-1][1];
+                D[i][0] = D[i-1][0] + D[i-1][1];
             }
             if (j == 1) {
-                D[i][j] = D[i-1][0];
+                D[i][1] = D[i-1][0];
             }
         }
     }
