95495daa5e020db00c2ac3837f144132b87553b9
Eliot Jang <eliotjang2@gmail.com>
2024-03-27 15:14:40 +0900

    add "가장 큰 증가하는 부분 수열" base code (Dynamic Programming)

---
+++ Baekjoon/DynamicProgramming1/11055.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // freopen("input.txt","r",stdin);
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (int i=0; i<N; i++) {
+        cin >> A[i];
+    }
+    vector<int> D(N);
+    // D[i] = max(D[j]) + 1
+    //  j < i, A[j] < A[i]
+    for (int i=0; i<N; i++) {
+        D[i] = 1;
+        for (int j=0; j<i; j++) {
+            if (A[j] < A[i] && D[i] < D[j]+1) {
+                D[i] = D[j] + 1;
+            }
+        }
+    }
+    cout << *max_element(D.begin(), D.end()) << endl;
+
+    return 0;
+}
