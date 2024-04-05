d63a282c11349758ece19f154841755d9fb4c362
Eliot Jang <eliotjang2@gmail.com>
2024-04-05 21:03:50 +0900

    test

--- Baekjoon/DynamicProgramming1/11055.cpp
+++ Baekjoon/DynamicProgramming1/11055.cpp
@@ -9,7 +9,7 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    // freopen("input.txt","r",stdin);
+    freopen("input.txt","r",stdin);
     int N;
     cin >> N;
     vector<int> A(N);
