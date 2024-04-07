cae09c2269b74d121a7a0e902fd0f124a82b6255
Eliot Jang <eliotjang2@gmail.com>
2024-04-07 22:16:34 +0900

    add "연속합" base code

---
+++ Baekjoon/DynamicProgramming1/1912.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // freopen("input.txt","r",stdin);
+    int N;
+    cin >> N;
+
+    return 0;
+}
+
