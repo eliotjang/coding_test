31cc44db077ff639f280ef253d033c38103bc8cb
Eliot Jang <eliotjang2@gmail.com>
2024-04-16 19:51:39 +0900

    solve "몫 구하기"

---
+++ Programmers/Introduction/day3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = num1 / num2;
+    return answer;
+}
+
+int main() {
+    freopen("input.txt","r",stdin);
+    int num1, num2;
+    cin >> num1 >> num2;
+    cout << solution(num1, num2) << endl;
+}
