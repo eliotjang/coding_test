8f2c2cf3cb5150f502a3c35015ebf72249e5b133
Eliot Jang <eliotjang2@gmail.com>
2024-04-17 09:45:09 +0900

    solve "동물의 아이디와 이름"

---
+++ Programmers/Introduction/day5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    
+    if (num1 == num2) {
+        answer = 1;
+    } else {
+        answer = -1;
+    }
+    
+    return answer;
+}
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    int num1, num2;
+    cin >> num1 >> num2;
+
+    cout << solution(num1, num2) << endl;
+
+    return 0;
+}
