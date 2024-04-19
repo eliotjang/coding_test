da392797b292583c30d6f0ac087a308cb2478ec8
Eliot Jang <eliotjang2@gmail.com>
2024-04-19 10:43:09 +0900

    solve "x만큼 간격이 있는 n개의 숫자"

---
+++ Programmers/Introduction/day16.cpp
@@ -0,0 +1,14 @@
+// x만큼 간격이 있는 n개의 숫자
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+    vector<long long> answer;
+    long long tmp = x;
+    for (int i=0; i<n; i++) {
+        answer.push_back(tmp);
+        tmp += x;
+    }
+    return answer;
+}
