5d049876b72e0b536dff2ef3c03535cc2341257b
Eliot Jang <eliotjang2@gmail.com>
2024-04-20 23:36:09 +0900

    solve "나이 출력"

--- Programmers/Introduction/day4.js
+++ Programmers/Introduction/day4.js
@@ -1,10 +1,6 @@
-// 숫자 비교하기
-function solution(num1, num2) {
+// 나이 출력
+function solution(age) {
     var answer = 0;
-    if (num1 == num2) {
-        answer = 1;
-    } else {
-        answer = -1;
-    }
+    answer = 2023 - age;
     return answer;
 }
