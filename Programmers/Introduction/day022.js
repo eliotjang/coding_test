cd08ff90d8ed9615497dea8b6b412c09e0b9cb76
Eliot Jang <eliotjang2@gmail.com>
2024-04-24 10:30:30 +0900

    solve 두 정수 사이의 합

---
+++ Programmers/Introduction/day022.js
@@ -0,0 +1,9 @@
+// 두 정수 사이의 합
+function solution(a, b) {
+    var answer = 0;
+    if (a > b) [a, b] = [b, a];
+    for (let i = a; i <= b; i++) {
+        answer += i;
+    }
+    return answer;
+}
