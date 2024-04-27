ad685cad033192686c5e30abf5600c142a707f7e
Eliot Jang <eliotjang2@gmail.com>
2024-04-27 18:12:17 +0900

    Revert "solve "오랜 기간 보호한 동물(2)""
    This reverts commit a78d809c426d61ffa89e5fe6924366de8e061016.

--- Programmers/Introduction/day029.js
+++
@@ -1,9 +0,0 @@
-// 제일 작은 수 제거하기
-function solution(arr) {
-    if (arr.length === 1) return [-1];
-    let minNum = Math.min(...arr);
-    let answer = arr.filter(e => {
-        return e !== minNum;
-    });
-    return answer;
-}
--- Programmers/SQL/day035.sql
+++
@@ -1,5 +0,0 @@
--- 오랜 기간 보호한 동물(2)
-select ao.animal_id, ao.name
-from animal_ins ai inner join animal_outs ao on ai.animal_id = ao.animal_id
-order by ao.datetime - ai.datetime desc
-limit 2
