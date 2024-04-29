1906a860129f1068a8f210af556679cef33b2548
Eliot Jang <eliotjang2@gmail.com>
2024-04-29 11:50:38 +0900

    solve bubble sort

--- Programmers/Introduction/bubble_sort.js
+++ Programmers/Introduction/bubble_sort.js
@@ -2,8 +2,7 @@ const bubbleSort = arr => {
     let sortArr = arr;
     for (let i = 0; i < sortArr.length - 1; i++) {
         for (let j = 0; j < sortArr.length - 1; j++) {
-            if (sortArr[j] > sortArr[j + 1])
-                [sortArr[j], sortArr[j + 1]] = [sortArr[j + 1], sortArr[j]];
+            if (sortArr[j] > sortArr[j + 1]) [sortArr[j], sortArr[j + 1]] = [sortArr[j + 1], sortArr[j]];
         }
         return sortArr;
     }
