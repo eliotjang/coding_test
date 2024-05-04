8b9cbb0e74d1b762982b20673771487a39b9c28f
Eliot Jang <eliotjang2@gmail.com>
2024-05-04 16:09:49 +0900

    solve "조건에 맞는 사용자와 총 거래금액 조회하기"

--- Programmers/SQL/day043.sql
+++ Programmers/SQL/day043.sql
@@ -1,10 +1,7 @@
 -- 조건에 맞는 사용자와 총 거래금액 조회하기
-select user_id, nickname, total_sales
-from
-(
-select user_id, nickname, sum(price) total_sales
+select user_id, nickname, sum(price) total_price
 from used_goods_board b inner join used_goods_user u on b.writer_id = u.user_id
+where status = 'done'
 group by 1
+having total_price >= 700000
 order by 3
-) a
-where total_sales >= 700000
