ada2fc605b0e49379c02bb8db3a1d91d212fe201
Eliot Jang <eliotjang2@gmail.com>
2024-05-19 14:22:54 +0900

    solve "숫자 문자열과 영단어"

--- Programmers/Introduction/day046.js
+++ Programmers/Introduction/day046.js
@@ -1,2 +1,55 @@
-//test
-//test
+// 숫자 문자열과 영단어
+function solution(s) {
+  let answer = "";
+  for (let i = 0; i < s.length; i++) {
+    switch (s[i]) {
+      case "z":
+        i += 3;
+        answer += "0";
+        break;
+      case "o":
+        i += 2;
+        answer += "1";
+        break;
+      case "t":
+        if (s[i] + s[i + 1] == "tw") {
+          i += 2;
+          answer += "2";
+        } else {
+          i += 4;
+          answer += "3";
+        }
+        break;
+      case "f":
+        if (s[i] + s[i + 1] == "fo") {
+          i += 3;
+          answer += "4";
+        } else {
+          i += 3;
+          answer += "5";
+        }
+        break;
+      case "s":
+        if (s[i] + s[i + 1] == "si") {
+          i += 2;
+          answer += "6";
+        } else {
+          i += 4;
+          answer += "7";
+        }
+        break;
+      case "e":
+        i += 4;
+        answer += "8";
+        break;
+      case "n":
+        i += 3;
+        answer += "9";
+        break;
+      default:
+        answer += s[i];
+        break;
+    }
+  }
+  return parseInt(answer);
+}
