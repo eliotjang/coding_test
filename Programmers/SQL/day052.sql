a993b25684e8c84f12fdb929735665cdcf6df988
Eliot Jang <eliotjang2@gmail.com>
2024-05-21 09:11:14 +0900

    solve "과일로 만든 아이스크림 고르기"

---
+++ Programmers/SQL/day052.sql
@@ -0,0 +1,5 @@
+-- 과일로 만든 아이스크림 고르기
+select ii.flavor
+from first_half fh inner join icecream_info ii on fh.flavor = ii.flavor
+where total_order > 3000 and ingredient_type = 'fruit_based'
+order by total_order desc
