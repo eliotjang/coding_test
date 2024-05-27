1976f938e1c95b983e1501e8470d47b395c915b1
Eliot Jang <eliotjang2@gmail.com>
2024-05-27 09:45:30 +0900

    solve "가장 가까운 같은 글자"

--- Programmers/Introduction/day050.js
+++ Programmers/Introduction/day050.js
@@ -1,1 +1,15 @@
-// test
+// 가장 가까운 같은 글자
+function solution(s) {
+  let answer = [];
+  const checkAlpha = new Map();
+  for (let i = 0; i < s.length; i++) {
+    if (checkAlpha.get(s[i]) === undefined) {
+      checkAlpha.set(s[i], i);
+      answer.push(-1);
+    } else {
+      answer.push(i - checkAlpha.get(s[i]));
+      checkAlpha.set(s[i], i);
+    }
+  }
+  return answer;
+}
