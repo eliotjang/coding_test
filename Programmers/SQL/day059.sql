5dbbae7f5fc7a399bc4f32ed3bf347aef10acaee
eliotjang <37320831+eliotjang@users.noreply.github.com>
2024-06-12 10:47:38 +0900

    refactor "자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기"

--- Programmers/SQL/day059.sql
+++ Programmers/SQL/day059.sql
@@ -1,7 +1,18 @@
 -- 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기
 select car_id,
-       case when car_id in (select car_id  from car_rental_company_rental_history where str_TO_DATE('20221016', '%Y%m%d') between start_date and end_date)  then '대여중'
+       case when car_id in (
+           select car_id  
+           from car_rental_company_rental_history 
+           where str_to_date('20221016', '%Y%m%d') between start_date and end_date
+       )  
+       then '대여중'
        else '대여 가능' end availability
-from (select distinct car_id from car_rental_company_rental_history) a
+from (
+    select distinct car_id 
+    from car_rental_company_rental_history
+) a
 group by 1
 order by 1 desc
+
+# 대여 중인 car_id 서브 쿼리
+# select * from car_rental_company_rental_history where str_to_date('20221016', '%Y%m%d') between start_date and end_date
