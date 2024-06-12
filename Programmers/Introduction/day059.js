9549b5c79770d67625e324142908b236272ca777
eliotjang <37320831+eliotjang@users.noreply.github.com>
2024-06-12 10:31:18 +0900

    solve "덧칠하기"

---
+++ Programmers/Introduction/day059.js
@@ -0,0 +1,12 @@
+// 덧칠하기
+function solution(n, m, sections) {
+  let answer = 0;
+  let painted = 0;
+  for (const section of sections) {
+    if (painted < section) {
+      answer++;
+      painted = section + m - 1;
+    }
+  }
+  return answer;
+}
