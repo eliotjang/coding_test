4628c1c2f0a47b3a745f3979d696e93bf6e6d80a
eliotjang <37320831+eliotjang@users.noreply.github.com>
2024-06-24 22:25:51 +0900

    solve "오프라인/온라인 판매 데이터 통합하기"

---
+++ Programmers/SQL/day071.sql
@@ -0,0 +1,24 @@
+-- 오프라인/온라인 판매 데이터 통합하기
+SELECT
+    DATE_FORMAT(sales_date, '%Y-%m-%d') sales_date
+  , product_id
+  , user_id
+  , sales_amount
+FROM
+    online_sale
+WHERE
+    DATE_FORMAT(sales_date, '%Y-%m') = '2022-03'
+UNION
+SELECT
+    DATE_FORMAT(sales_date, '%Y-%m-%d') sales_date
+  , product_id
+  , NULL user_id
+  , sales_amount
+FROM
+    offline_sale
+WHERE
+    DATE_FORMAT(sales_date, '%Y-%m') = '2022-03'
+ORDER BY
+    sales_date
+  , product_id
+  , user_id
