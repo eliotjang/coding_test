41c89bfd06c311700f5930ad5187ec373bda4a3e
eliotjang <37320831+eliotjang@users.noreply.github.com>
2024-09-10 15:50:57 +0900

    Create flag에 따라 다른 값 반환하기.cpp

---
+++ Programmers/basic-coding-training/연산/flag에 따라 다른 값 반환하기.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, bool flag) {
+    int answer = 0;
+    if (flag) {
+        answer = a + b;
+    } else {
+        answer = a - b;
+    }
+    return answer;
+}
+
+// another person's solution : use ternery operator
