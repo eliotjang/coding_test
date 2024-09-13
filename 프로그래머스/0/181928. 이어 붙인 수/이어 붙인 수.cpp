4c50a217eb6889dbcd9b74d22fb902e1811a0b98
Eliot Jang <37320831+eliotjang@users.noreply.github.com>
2024-09-13 09:15:42 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    string odd, even;
+    
+    for (int i = 0; i < num_list.size(); i++) {
+        num_list[i] % 2 != 0 ? odd.append(1, static_cast<char>(num_list[i] + '0')) : even.append(1, static_cast<char>(num_list[i] + '0'));
+    }
+    return stoi(odd) + stoi(even);
+}
