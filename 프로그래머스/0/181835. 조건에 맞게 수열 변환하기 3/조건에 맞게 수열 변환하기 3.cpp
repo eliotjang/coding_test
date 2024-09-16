413c1bec09f7555edcb9c31b63f5f231548bb410
Eliot Jang <37320831+eliotjang@users.noreply.github.com>
2024-09-16 14:23:36 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 307.12 ms, Memory: 224 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.cpp
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.cpp
@@ -5,11 +5,7 @@ using namespace std;
 vector<int> solution(vector<int> arr, int k) {
     vector<int>::iterator it;
     for (it = arr.begin(); it != arr.end(); ++it) {
-        if (k % 2 == 0) {
-            *it += k;
-        } else {
-            *it *= k;
-        }
+        k % 2 == 0 ? *it += k : *it *= k;
     }
     return arr;
 }
