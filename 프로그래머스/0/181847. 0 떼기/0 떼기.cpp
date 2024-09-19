bcd33e9ac12b820356d2e550af473ebcab63c4d6
Eliot Jang <37320831+eliotjang@users.noreply.github.com>
2024-09-19 16:29:30 +0900

    [level 0] Title: 0 떼기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+
+using namespace std;
+
+string solution(string n_str) {
+    string answer = "";
+    int pos = 0;
+    for (char c : n_str) {
+        if (c != '0') break;
+        pos++;
+    }
+    answer = n_str.substr(pos);
+    return answer;
+}
