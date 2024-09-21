8c87ee6a90917cebfe7dc9a865abd7890984994e
Eliot Jang <37320831+eliotjang@users.noreply.github.com>
2024-09-21 23:46:31 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.cpp
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -1,8 +1,11 @@
+#include <string>
+#include <cmath>
+
 using namespace std;
 
 int solution(int a, int b) {
     if (a % 2 != 0 && b % 2 != 0) {
-        return a * a + b * b;
+        return pow(a, 2) + pow(b, 2);
     }
     else if (a % 2 != 0 || b % 2 != 0) {
         return 2 * (a + b);
