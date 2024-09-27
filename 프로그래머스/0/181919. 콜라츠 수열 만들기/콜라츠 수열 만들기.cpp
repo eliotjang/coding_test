48cee444b999bcb9e9fe8c3a503ebf5c4d74769f
Eliot Jang <37320831+eliotjang@users.noreply.github.com>
2024-09-27 10:22:30 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.04 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    while (n != 1) {
+        answer.push_back(n);
+        n % 2 == 0 ? n /= 2 : n = n * 3 + 1;
+    }
+    answer.push_back(1);
+    return answer;
+}
