c346381f56ac87c02be9e3ee3af71d96f09b871f
Eliot Jang <37320831+eliotjang@users.noreply.github.com>
2024-10-07 15:41:08 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+    answer.insert(answer.begin(), num_list.begin() + n, num_list.end());
+    answer.insert(answer.end(), num_list.begin(), num_list.begin() + n);
+    return answer;
+}
