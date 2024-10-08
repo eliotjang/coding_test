ec268cddaca9a92528995d2ad02da626391a8390
Eliot Jang <37320831+eliotjang@users.noreply.github.com>
2024-10-08 10:52:28 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <numeric> // accumulate
+#include <functional> // plus, multiplies
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    if (num_list.size() >= 11) {
+        answer = accumulate(num_list.begin(), num_list.end(), 0, std::plus<int>());
+    } else {
+        answer = accumulate(num_list.begin(), num_list.end(), 1, std::multiplies<int>());
+    }
+    return answer;
+}
