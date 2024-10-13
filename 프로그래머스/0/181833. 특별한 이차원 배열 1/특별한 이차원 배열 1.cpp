86b0f1f7753e2179b2e0f308775d531fba8500c2
Eliot Jang <37320831+eliotjang@users.noreply.github.com>
2024-10-13 18:07:39 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 3.41 ms, Memory: 5.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(int n) {
+    vector<vector<int>> answer(n, vector<int>(n, 0));
+    for (int i = 0; i < answer.size(); i++) {
+        answer[i][i] = 1;
+    }
+    return answer;
+}
