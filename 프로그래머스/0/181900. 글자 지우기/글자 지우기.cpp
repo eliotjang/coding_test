27fafd8b6672a9700e1bae5f31bbdaa50c69eb7b
Eliot Jang <37320831+eliotjang@users.noreply.github.com>
2024-10-15 17:16:43 +0900

    [level 0] Title: 글자 지우기, Time: 0.01 ms, Memory: 3.74 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, vector<int> indices) {
+    char delimeter = '*';
+    for (vector<int>::iterator it = indices.begin(); it != indices.end(); ++it) {
+        my_string.at(*it) = delimeter;
+    }
+    size_t found = my_string.find(delimeter);
+    while (found != string::npos) {
+        my_string.erase(found, 1);
+        found = my_string.find(delimeter);
+    }
+    return my_string;
+}
