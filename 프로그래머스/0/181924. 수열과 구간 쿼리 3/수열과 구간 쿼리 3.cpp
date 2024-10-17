fbc1fdcbd3a249d0e23595bd9c766e58020cda45
Eliot Jang <37320831+eliotjang@users.noreply.github.com>
2024-10-17 14:26:03 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.41 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <utility> // swap
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+    for (vector<int> q : queries) {
+        swap(arr[q[0]], arr[q[1]]);
+    }
+    return arr;
+}
