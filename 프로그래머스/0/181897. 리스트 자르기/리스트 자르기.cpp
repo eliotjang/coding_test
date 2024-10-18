13f0f6ca9ea164f3aca9f149ebf9fc54ec4b08c0
Eliot Jang <37320831+eliotjang@users.noreply.github.com>
2024-10-18 19:00:16 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, vector<int> slicer, vector<int> num_list) {
+    int a = slicer[0], b = slicer[1], c = slicer[2];
+    switch (n) {
+        case 1:
+            return vector<int> (num_list.begin(), num_list.begin() + b + 1);
+        case 2:
+            return vector<int> (num_list.begin() + a, num_list.end());
+        case 3:
+            return vector<int> (num_list.begin() + a, num_list.begin() + b + 1);
+        case 4:
+            vector<int> answer;
+            for (int i = a; i <= b; i += c) { answer.push_back(num_list[i]); }
+            return answer;
+    }
+}
