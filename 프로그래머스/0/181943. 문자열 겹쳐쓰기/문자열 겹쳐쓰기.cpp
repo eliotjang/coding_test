519f1ac214fd53d4d1e254bac23e53faa613f1a1
Eliot Jang <37320831+eliotjang@users.noreply.github.com>
2024-10-23 20:30:43 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,7 @@
+#include <string>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    return my_string.replace(s, overwrite_string.size(), overwrite_string);
+}
